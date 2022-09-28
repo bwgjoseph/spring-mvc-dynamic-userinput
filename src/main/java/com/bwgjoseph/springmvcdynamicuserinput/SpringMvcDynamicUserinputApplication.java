402d8f9b4db85c495caafef4b5a1c84d1e50df38
bwgjoseph <josephgan@live.com.sg>
2022-09-28 16:19:54 +0800

    Initial Commit

---
+++ src/main/java/com/bwgjoseph/springmvcdynamicuserinput/SpringMvcDynamicUserinputApplication.java
@@ -0,0 +1,13 @@
+package com.bwgjoseph.springmvcdynamicuserinput;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcDynamicUserinputApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcDynamicUserinputApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bwgjoseph/springmvcdynamicuserinput/SpringMvcDynamicUserinputApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bwgjoseph.springmvcdynamicuserinput;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcDynamicUserinputApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
